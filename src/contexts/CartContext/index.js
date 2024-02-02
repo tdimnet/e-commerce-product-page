ccb548204d18b8ca081926b39d6713af97168eba
tdimnet <thomas.dimnet@gmail.com>
2024-02-02 18:13:39 +0100

    Creating Cart Context

---
+++ src/contexts/CartContext/index.js
@@ -0,0 +1,35 @@
+import {
+    createContext,
+    useState,
+    useContext
+} from 'react'
+
+const CartContext = createContext()
+
+function useCart() {
+    return useContext(CartContext)
+}
+
+function CartProvider({ children }) {
+    const [cart, setCart] = useState([])
+
+    function addToCart(product) {
+        setCart([...cart, product])
+    }
+
+    function removeFromCart(productId) {
+        const updatedCart = cart.filter(item => item.id !== productId)
+        setCart(updatedCart)
+    }
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export {
+    useCart,
+    CartProvider
+}
