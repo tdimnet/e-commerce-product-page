58d40bcaa6da9023e719af4201554b43e00c999f
tdimnet <thomas.dimnet@gmail.com>
2024-02-02 18:49:00 +0100

    Creating new stories

---
+++ src/components/Counter/index.stories.js
@@ -0,0 +1,13 @@
+import Counter from "."
+
+export default {
+    title: "Components/Counter",
+    component: Counter
+}
+
+export const Default = {
+    args: {
+        onClick: () => {},
+        counter: 2
+    }
+}
---
+++ src/components/Discount/index.stories.js
@@ -0,0 +1,12 @@
+import Discount from "."
+
+export default {
+    title: "Components/Discount",
+    component: Discount
+}
+
+export const Default = {
+    args: {
+        discountValue: 30
+    }
+}
