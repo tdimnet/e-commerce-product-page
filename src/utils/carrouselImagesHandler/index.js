2573b110117c39dc935235fee02396da10c230af
tdimnet <thomas.dimnet@gmail.com>
2024-02-10 10:17:38 +0100

    Writting the test cases

--- src/utils/carrouselImagesHandler/index.js
+++ src/utils/carrouselImagesHandler/index.js
@@ -1,4 +1,21 @@
 function util(numberOfImages, currentImage, action) {
+    let newIndex = currentImage
+
+    if (action === "next") {
+        if ((currentImage + 1) === numberOfImages) {
+            newIndex = 0
+        } else {
+            newIndex++
+        }
+    } else if (action === "previous") {
+        if ((currentImage - 1) === -1) {
+            newIndex = numberOfImages - 1
+        } else {
+            newIndex--
+        }
+    }
+    
+    return newIndex
 }
 
 export default util
--- src/utils/carrouselImagesHandler/index.test.js
+++ src/utils/carrouselImagesHandler/index.test.js
@@ -8,7 +8,27 @@ import carrouselImageHandler from "."
 
 describe('carrouselImageHandler Unit Test Suites', () => {
     it('should return something', () => {
-        expect(true).toBe(true)
+        expect(carrouselImageHandler(4, 0, 'next')).toBeDefined()
+    })
+
+    it('should return a number', () => {
+        expect(typeof carrouselImageHandler(4, 0, 'next')).toBe('number')
+    })
+
+    it('should return the next index - 1 if currentIndex is 0', () => {
+        expect(carrouselImageHandler(4, 0, 'next')).toBe(1)
+    })
+
+    it('should return the previous index - 2 if currentIndex is 3', () => {
+        expect(carrouselImageHandler(4, 3, 'previous')).toBe(2)
+    })
+
+    it('should return the start index if the array is at the end', () => {
+        expect(carrouselImageHandler(4, 3, 'next')).toBe(0)
+    })
+
+    it('should return the the end infex if the array is at the beginning', () => {
+        expect(carrouselImageHandler(4, 0, 'previous')).toBe(3)
     })
 })
 
