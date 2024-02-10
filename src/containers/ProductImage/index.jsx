719992c554bb2ccc477c483105b9a06ac4068ced
tdimnet <thomas.dimnet@gmail.com>
2024-02-10 09:44:16 +0100

    Starting to add the carrousel logic

--- src/containers/ProductImage/index.jsx
+++ src/containers/ProductImage/index.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 import ProductImage from "../../components/ProductImage";
 import ArrowIcon from "../../icons/Arrow";
 
-import image from "../../images/image-product-1.jpg";
+import firstImage from "../../images/image-product-1.jpg";
+import secondImage from "../../images/image-product-2.jpg";
+import thirdImage from "../../images/image-product-3.jpg";
+import fourthImage from "../../images/image-product-4.jpg";
+
+const IMAGES = [firstImage, secondImage, thirdImage, fourthImage]
 
 function Container() {
-  function handleIcons(kind) {
+  const [ image, setImage ] = useState(IMAGES[0])
+
+  function onChangeImage(kind) {
     console.log("=====");
     console.log(kind);
     console.log("=====");
@@ -13,14 +22,14 @@ function Container() {
   return (
     <div className="relative">
       <ArrowIcon
-        css="absolute left-0 top-[50%] bg-white rounded-full h-10 w-10"
-        onClick={() => handleIcons("previous")}
+        css="absolute left-0 top-[50%] bg-white rounded-full h-10 w-10 flex items-center justify-center"
+        onClick={() => onChangeImage("previous")}
         kind="previous"
       />
       <ProductImage src={image} />
       <ArrowIcon
-        css="absolute right-0 top-[50%] bg-white rounded rounded-full h-10 w-10"
-        onClick={() => handleIcons("next")}
+        css="absolute right-0 top-[50%] bg-white rounded rounded-full h-10 w-10 flex items-center justify-center"
+        onClick={() => onChangeImage("next")}
       />
     </div>
   );
