00f1044de0bfe6af14296a8248e4eaf35775e05b
tdimnet <thomas.dimnet@gmail.com>
2024-02-22 09:54:31 +0100

    Trying to deploy project with GH Actions

--- src/components/ProductName/index.jsx
+++ src/components/ProductName/index.jsx
@@ -1,5 +1,5 @@
 function Component({ name, css }) {
-  return <h1 className={`${css} text-3xl font-bold capitalize`}>{name}</h1>;
+  return <h1 className={`${css} text-3xl font-bold capitalize md:text-4xl`}>{name}</h1>;
 }
 
 export default Component;
